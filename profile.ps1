55a2064f84e32fe4902f89722f3f623acd5bc84f
bukowskim <mateusz.bukowski@britishcouncil.org>
2019-01-28 11:22:23 +0100

    added profile powershell code

---
+++ profile.ps1
@@ -0,0 +1,52 @@
+function Test-IslocalAdministrator() {
+	$currentwindowsidentity = [system.security.principal.windowsidentity]::getcurrent()
+	$currentwindowsprincipal = new-object system.security.principal.windowsprincipal($currentwindowsidentity)
+	$isinwindowsbuiltinadministratorrole = $currentwindowsprincipal.isinrole([system.security.principal.windowsbuiltinrole]::administrator)
+	return $isinwindowsbuiltinadministratorrole;
+}
+
+function New-OrUpdateWindowsCredential() {
+	$fileName = (Join-Path $env:userprofile "credentials.xml");
+	if (Test-Path $fileName) 
+	{
+	  Remove-Item $fileName
+	}
+
+	$credentials = Get-Credential
+	$credentials | Export-Clixml $fileName
+}
+
+function Start-WithAdmin {
+	Param(
+         [Parameter(Mandatory=$true, Position=0)]
+         [string] $filePath
+    )
+	
+	if(!(Test-IslocalAdministrator)){
+		$credentials = Import-CliXml -Path (Join-Path $env:userprofile "credentials.xml")
+		Start-Process -PassThru powershell -Windowstyle hidden -Credential $credentials -ArgumentList '-noprofile -command &{Start-Process -FilePath ''',  $filePath, ''' -verb runas}' | Out-Null
+	}
+	else{
+		Start-Process -FilePath $filePath
+	}
+}
+
+function Start-VisualStudio {
+	Start-WithAdmin "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.exe"
+}
+
+function Set-AdminShortcut {
+	param ( 
+		[Parameter(Mandatory=$true)][string]$sourceExe, 
+		[Parameter(Mandatory=$true)][string]$destShortcut,
+		[Parameter(Mandatory=$false)][string]$command = "Start-WithAdmin '"+ $sourceExe + "'"
+	)
+	
+	$WshShell = New-Object -comObject WScript.Shell
+	$Shortcut = $WshShell.CreateShortcut($destShortcut);
+	$Shortcut.TargetPath = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe";
+	$Shortcut.Arguments = "-Command " + $command;
+	$Shortcut.IconLocation = $sourceExe;
+	$Shortcut.WindowStyle = 7;
+	$Shortcut.Save();
+}
